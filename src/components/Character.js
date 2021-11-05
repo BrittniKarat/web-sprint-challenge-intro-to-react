4589d5fc6fa9f9c3d6acca8ab9566dd70b4af097
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-11-05 00:58:48 -0700

    Created state, built out Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterStyles = styled.div`
+    color:white;
+`
+
+
+const CharacterData = (props) => {
+    return (
+        <CharacterStyles>
+            {props.character}
+        </CharacterStyles>
+    )
+}
+
+
+export default CharacterData;
